02c976d7921e78afdb97f1de33fcf10098074754
LarisaZaripova <larik7lolik@gmail.com>
2022-05-26 13:43:09 +0300

    added user.py

---
+++ user.py
@@ -0,0 +1,9 @@
+
+def initial():
+    surname = input('Введите фамилию: ')
+    name = input('Введите имя: ')
+    number_telephon = input('Введите номер телефона: ')
+    comment = input('Введите комментарий: ')
+    text_orientation = input('Выбрать ориентацию (1: вертикальную, 2: горизонтальную): ')
+    return(surname,  name, number_telephon,  comment, text_orientation)
+
